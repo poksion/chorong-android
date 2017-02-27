794e49a3d3290e7715a630061356637d099df424
poksion <poksion@gmail.com>
2017-02-27 17:19:11 +0900

    refine google sheet api test

--- chorong-api/src/test/java/net/poksion/chorong/android/api/GoogleSheetApiTest.java
+++ chorong-api/src/test/java/net/poksion/chorong/android/api/GoogleSheetApiTest.java
@@ -7,10 +7,11 @@ import org.junit.Test;
 public class GoogleSheetApiTest {
 
     @Test
-    public void testInvalidToken() {
+    public void result_should_be_empty_without_valid_token() {
         GoogleSheetApi googleSheetApi = new GoogleSheetApiImpl("chorong-api-test");
         GoogleSheetApi.Result result = googleSheetApi.getValues("", "dummy-name", "dummy-sheet-name", -1, -1, -1);
 
         assertThat(result.validToken).isFalse();
+        assertThat(result.rows).isEmpty();
     }
 }
